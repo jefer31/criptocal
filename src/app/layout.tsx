67069a080352f1be48540602861ee051a3dd1453
Jefer31 <jefer31@example.com>
2026-06-12 03:22:30 -0400

    UI: Implementar dual-icon (cuadrado para Android, redondeado para PC/Pestañas)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="icon" href="/logo-rounded.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo-rounded.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
